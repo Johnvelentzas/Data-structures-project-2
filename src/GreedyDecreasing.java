a4cbfee090dfa92ed11e31bbbfdc7156d92834c5
Johnvelentzas <yiannis.velentzas@gmail.com>
2022-12-30 20:06:13 +0200

    added greedy decreasing

--- src/GreedyDecreasing.java
+++ src/GreedyDecreasing.java
@@ -1,5 +1,19 @@
 public class GreedyDecreasing{
     public static void main(String args[]){
-
+        System.out.println("Starting greedy-decreasing algorithm");
+        /*
+         * Creates a parser and extracts the data from the txt file.
+         * If the parser can't extract the data it returns closing the main method;
+         */
+        foldersTXTparser parserData;
+        try {
+            parserData = new foldersTXTparser("src\\folders.txt");
+        } catch (Exception e) {
+            System.out.println("Couldn't read file.");
+            return;
+        }
+        Folder[] Folders = parserData.getSerialQueue();
+        Folders = Sort.sortIntoPriorityQueue(Folders);
+        Greedy.GreedyAlgorithm(Folders);
     }
 }
--- src/Sort.java
+++ src/Sort.java
@@ -1,5 +1,15 @@
-public class Sort<T> {
-    public MaxPQ<T> sortIntoPriorityQueue(QueueImpl<T> queue){
-        return null;
+public class Sort {
+    public static Folder[] sortIntoPriorityQueue(Folder[] folders){
+            Folder temp;
+            for (int i = 0; i < folders.length - 1; i++) {
+                for (int j = i + 1; j < folders.length; j++) {
+                    if (folders[j].compareTo(folders[i]) == 1) {
+                        temp = folders[i];
+                        folders[i] = folders[j];
+                        folders[j] = temp;
+                    }
+                }
+            }
+        return folders;
     }
 }
